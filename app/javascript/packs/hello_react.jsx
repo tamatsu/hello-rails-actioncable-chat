8ca6f315b8d2b2c0d5aa38be2fe91f062e56698a
Tamaz <gtamatsu@gmail.com>
2021-01-07 17:05:55 +0900

    Create View

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -6,21 +6,72 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+const domContainer = document.querySelector('#app');
 
-Hello.defaultProps = {
-  name: 'David'
+
+// React
+const e = React.createElement;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = init()
+  }
+
+  render() {
+    return view({ model: this.state, msg: this.msg.bind(this)})
+  }
+
+  msg(f, args) {
+    this.setState(f({ model: this.state, args }))
+  }
 }
 
-Hello.propTypes = {
-  name: PropTypes.string
+ReactDOM.render(e(App), domContainer);
+
+
+// Model
+function init() {
+  return {
+    items: [],
+    newText: ''
+  }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+function add({ model }) {
+  const item = {
+    id: uuidv4(),
+    content: model.newText
+  }
+
+  model.newText = ''
+  model.items.push(item)
+
+  return model
+}
+
+function inputNewText({ model, args: value }) {
+  model.newText = value
+
+  return model
+}
+
+// View
+function view({ model, msg }) {
+  return <div>
+    { console.log('->', model), ''}
+    <form onClick={e => { e.preventDefault(); msg(add) }}>
+      <input value={model.newText} onInput={e => msg(inputNewText, e.target.value)} />
+      <button>+</button>
+    </form>
+    <div>
+      { model.items.map(item => viewItem({ item })) }
+    </div>
+  </div>
+}
+
+function viewItem({ item }) {
+  return <div>
+    { item.content }
+  </div>
+}
